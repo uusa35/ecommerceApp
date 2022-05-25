cc1c6c7522b7212a2d931732445a454e1c92d0eb
uusa35 <uusa35@gmail.com>
2022-05-25 15:56:30 +0300

    updates

--- app/Constants/index.js
+++ app/Constants/index.js
@@ -2,4 +2,4 @@ import React from 'react';
 import {Platform, Dimensions} from 'react-native';
 export const {height, width} = Dimensions.get('window');
 export const isIOS = Platform.OS === 'ios' ? true : false;
-export const appName = 'istores';
+export const appName = 'ecommerce';
